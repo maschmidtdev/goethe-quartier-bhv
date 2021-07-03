eb1b0b84a6c1c1a640904b5aec4176cf5096f342
Marcel <maschmidt.dev@gmail.com>
2021-07-03 19:43:49 +0200

    studentisches wohnen page 2 und 3

--- src/components/details/StudentischesWohnen.js
+++ src/components/details/StudentischesWohnen.js
@@ -44,10 +44,75 @@ export default function StudentischesWohnen() {
                 );
 
             case 2:
-                return <h1>Page 2</h1>;
+                return (
+                    <div className='container'>
+                        <div className='combine'>
+                            <img src={StudentischesWohnen_2} alt='' />
+                            <img src={StudentischesWohnen_4} alt='' />
+                        </div>
+                        <p>
+                            Um Erdegschoss-Zonen vor allem gemeinschaftlich
+                            und/oder gewerblich besser nutzen zu können, musste
+                            in manchen Fällen das Treppenhaus von dem direkten
+                            Zugang zum Straßenraum getrennt werden. Der direkte
+                            Zugang ins Freie wird durch eine Hoferschließung
+                            gewährleistet, der direkte Zugang in die
+                            Treppenhäuser erfolgt dann also durch die Durchfahrt
+                            in der Eupener Straße und einen attraktiven
+                            Innenhof. Somit können ebenerdig zum öffentlichen
+                            Raum eine Gastronomie im Eckgebäude und das
+                            studentische Coworking von der Goethestraße aus
+                            betreten werden. Rund um die Uhr zugängliche
+                            studentische Arbeitsplätze entlasten die
+                            Hochschulräumlichkeiten, bringen das Arbeiten und
+                            Lernen näher an den Lebensmittelpunkt und schaffen
+                            zusätzliche gemeinschaftliche Flächen. Die
+                            Gastronomie kann man sich in diesem arbeitsreichen
+                            Kontext als „Mensa für alle“ vorstellen, das ganze
+                            Quartier soll von dem zusätzlichen kulinarischen
+                            Angebot profitieren.
+                        </p>
+                    </div>
+                );
 
             case 3:
-                return <h1>Page 3</h1>;
+                return (
+                    <div className='container'>
+                        <div className='combine'>
+                            <img src={StudentischesWohnen_3} alt='' />
+                            <img src={StudentischesWohnen_4} alt='' />
+                        </div>
+                        <div className='text'>
+                            <p>
+                                Normalerweise wird es ab dem 1. Obergeschoss
+                                etwas ruhiger. Nicht so in diesem Fall. Im
+                                Eckgebäude findet klassisches Co-Working für
+                                jedermann statt, Startups und Jungunternehmer
+                                sollen von gemeinsam genutzten Räumlichkeiten
+                                und Einrichtungen profitieren. Teilen,
+                                voneinander lernen, in der Gemeinschaftsküche
+                                noch eben ein Schnack über die neuesten
+                                Entwicklungen in der IT-Welt: Niemand ist hier
+                                alleine. Ähnlich lebendig dürfte es im
+                                Gebäudeteil an der Goethestraße zugehen, wo die
+                                ehem. Einzelwohnungen zu größeren
+                                Wohngemeinschaften für Studierende umgebaut
+                                werden sollen. Das klassische Pendant hierzu
+                                wird in dem kleineren Abschnitt in der Eupener
+                                Straße anegboten, wo geschossübergreifend 2- bis
+                                3-Zimmer-Wohnungen angeboten werden - möglichst
+                                auch für Studierende vorbehalten. (Hinweis: Die
+                                Erschließung über den Hof ist noch nicht
+                                brandschutztechnisch überprüft)
+                            </p>
+                            <br />
+                            <p>
+                                (Hinweis: Die Erschließung über den Hof ist noch
+                                nicht brandschutztechnisch überprüft)
+                            </p>
+                        </div>
+                    </div>
+                );
 
             default:
                 break;
