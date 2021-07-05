408e9c30653bf590f1631c088e5e6c25ea25fa9f
Marcel <maschmidt.dev@gmail.com>
2021-07-05 23:55:04 +0200

    startseite texte

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import { Videosource } from '../assets/index';
 import Video from '../components/Video';
 
 export default function Home() {
-    const [page, setPage] = useState(1);
+    const [page, setPage] = useState(0);
     const [isShow, setIsShow] = useState(false);
 
     const getContent = (page, show) => {
@@ -11,14 +11,16 @@ export default function Home() {
             case 1:
                 return (
                     <div className={`${show ? 'home-flex show' : 'home-flex'}`}>
+                        <p>Was passiert,</p>
                         <p>
-                            Was passiert, wenn sich zwei MasterstudentInnen der
-                            Architektur mit dem
+                            wenn sich zwei MasterstudentInnen der Architektur
+                            mit dem
                         </p>
                         <h1>G O T H E Q U A R T I E R</h1>
                         <p>
-                            in <em>Bremerhaven-Lehe</em> auseinandersetzen?...
+                            in <em>Bremerhaven-Lehe</em>
                         </p>
+                        <p>auseinandersetzen?...</p>
                         <button onClick={(e) => changePage(2)}>
                             klicke hier
                         </button>
@@ -27,12 +29,20 @@ export default function Home() {
             case 2:
                 return (
                     <div className={`${show ? 'home-flex show' : 'home-flex'}`}>
+                        <p>Wir wollen Lust machen auf einen</p>
+                        <br />
                         <p>
-                            Wir wollen Lust machen auf einen{' '}
-                            <strong>Lebensraum</strong> mit
-                            <strong> attraktiven Zukunftsperspektiven</strong>
-                            ...
+                            einen <strong>Lebensraum</strong> mit
                         </p>
+                        <br />
+                        <p>
+                            <strong> attraktiven</strong>
+                        </p>
+                        <br />
+                        <p>
+                            <strong>Zukunftsperspektiven</strong>...
+                        </p>
+                        <br />
 
                         <button
                             onClick={(e) => {
@@ -40,19 +50,23 @@ export default function Home() {
                                 changePage(3);
                             }}
                         >
-                            okay dann zeig her!
+                            okay, dann zeig her!
                         </button>
                     </div>
                 );
             case 3:
                 return (
                     <div className={`${show ? 'home-flex show' : 'home-flex'}`}>
+                        <p>Wir zeigen dir Projekte und Ideen,</p>
+                        <br />
+                        <p>die das Quartier in ihrer</p>
+                        <br />
                         <p>
-                            Wir zeigen dir Projekte und Ideen, die das Quartier
-                            in ihrer{' '}
-                            <strong>nachhaltigen Entwicklung stärken</strong>{' '}
-                            sollen...
+                            <strong>nachhaltigen Entwicklung stärken</strong>
                         </p>
+                        <br />
+                        <p>sollen...</p>
+                        <br />
 
                         <a href='/map'>Kann ich jetzt zu den Projekten?</a>
                     </div>
@@ -74,9 +88,9 @@ export default function Home() {
 
     return (
         <div id='home'>
-            {/* <div className='home-video'>
+            <div className='home-video'>
                 <Video src={Videosource} auto={false} changePage={changePage} />
-            </div> */}
+            </div>
             {page > 0 && getContent(page, isShow)}
         </div>
     );
